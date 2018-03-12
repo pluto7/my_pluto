a8ad8dcc72ec03511f71b578ad04c608a847bce0
DESKTOP-QKTACQO\pluto <pluto7@nate.com>
2018-03-12 16:58:41 +0900

    최초 등록

---
+++ src/main/java/com/pluto/MyPlutoApplication.java
@@ -0,0 +1,12 @@
+package com.pluto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPlutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPlutoApplication.class, args);
+	}
+}
