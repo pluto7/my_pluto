add50dd58713e7293e81864b9e8a8f0dedbf638a
DESKTOP-QKTACQO\pluto <pluto7@nate.com>
2018-03-28 15:04:18 +0900

    4-5 강좌 완료

--- src/main/java/com/pluto/web/WelcomeController.java
+++ src/main/java/com/pluto/web/WelcomeController.java
@@ -1,14 +1,23 @@
 package com.pluto.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.pluto.domain.QuestionRepository;
+
 @Controller
 public class WelcomeController {
+	@Autowired
+	private QuestionRepository questionRepository;
+	
 	@GetMapping("/")
-	public String welcome() {
+	public String welcome(Model model) {
 		System.out.println("index 잘 넘어오나?");
+		model.addAttribute("question", questionRepository.findAll());
 		return "index";
 	}
+	
+	
 }
